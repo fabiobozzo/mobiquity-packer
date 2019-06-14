88a69e2ed4dd6b96bc16a5ec2846580f44451df8
Fabio Bozzo <fabio.bozzo@gmail.com>
2019-06-14 02:33:08 +0200

    unit test and model init

--- src/main/java/com/mobiquityinc/packer/Packer.java
+++ src/main/java/com/mobiquityinc/packer/Packer.java
@@ -1,5 +1,12 @@
 package com.mobiquityinc.packer;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.mobiquityinc.exception.APIException;
 
 /**
@@ -9,6 +16,31 @@ import com.mobiquityinc.exception.APIException;
 public class Packer {
     
     public static String pack(String filePath) throws APIException {
+    	
+    	Path path = Paths.get(filePath);
+    	
+    	try {
+			
+    		List<String> lines = Files.lines(path).collect(Collectors.toList());
+    		
+    		StringBuffer sb = new StringBuffer();
+    		
+    		for ( String l : lines ) {
+    			sb.append(Packer.process(l)).append("\n");
+    		}
+    		
+    		return sb.toString();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	return null;
+    }
+    
+    public static String process(String testCase) throws APIException {
+    	
+    	System.out.println(testCase);
     	
     	return null;
     }
--- src/test/java/com/mobiquityinc/packer/PackerTest.java
+++ src/test/java/com/mobiquityinc/packer/PackerTest.java
@@ -1,37 +1,66 @@
 package com.mobiquityinc.packer;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mobiquityinc.exception.APIException;
+
 
 /**
  * Unit test for Packer
  */
-public class PackerTest extends TestCase
+public class PackerTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public PackerTest( String testName )
-    {
-        super( testName );
+	private List<String> inputLines;
+	private List<String> outputLines;
+	
+	@Before
+    public void setup() {
+		inputLines = readLinesFromResource("input-sample.txt");
+		outputLines = readLinesFromResource("output-sample.txt");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( PackerTest.class );
+	@Test
+    public void testResources() {
+		assertNotNull(inputLines);
+		assertNotNull(outputLines);
+		assertEquals(inputLines.size(), outputLines.size());
+	}
+	
+    @Test
+    public void testPacker() {
+    	
+    	for ( int i = 0; i < inputLines.size(); i++ ) {
+    		try {
+    			
+				assertEquals( outputLines.get(i), Packer.process(inputLines.get(i)) );
+				
+			} catch (APIException e) {
+				fail(e.getMessage());
+			}
+    	}
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    
+    private List<String> readLinesFromResource(String filename) {
+    	ClassLoader  loader = PackerTest.class.getClassLoader();
+    	List<String> lines;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+			   loader.getResourceAsStream(filename), "UTF-8"
+			));
+			lines = br.lines().parallel().map(Object::toString).collect(Collectors.toList()); 
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			lines = null;
+		}	
+		return lines;
     }
 }
